afbfc2f0cedcdd2d8bf4abe3be1887bdeaaab999
Pavan Sokke Nagaraj <pavansn8@gmail.com>
2025-08-06 09:33:49 -0400

    chore: go fmt

--- cmd/provider/run/run.go
+++ cmd/provider/run/run.go
@@ -6,6 +6,7 @@ import (
 	"path/filepath"
 	"time"
 
+	"github.com/alecthomas/kingpin/v2"
 	xpv1 "github.com/crossplane/crossplane-runtime/apis/common/v1"
 	xpcontroller "github.com/crossplane/crossplane-runtime/pkg/controller"
 	"github.com/crossplane/crossplane-runtime/pkg/feature"
@@ -15,7 +16,6 @@ import (
 	tjcontroller "github.com/crossplane/upjet/pkg/controller"
 	"github.com/crossplane/upjet/pkg/terraform"
 	"github.com/go-logr/logr"
-	"github.com/alecthomas/kingpin/v2"
 	kerrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/tools/leaderelection/resourcelock"
