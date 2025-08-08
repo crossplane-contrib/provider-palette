8ca0a4cd023612f53078f58c88240ceb0f9c8413
Pavan Sokke Nagaraj <pavansn8@gmail.com>
2025-08-08 14:49:59 -0400

    go gen apis

---
+++ apis/cluster/v1beta1/register.go
@@ -0,0 +1,29 @@
+/*
+Copyright 2022 Upbound Inc.
+*/
+
+package v1beta1
+
+import (
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"sigs.k8s.io/controller-runtime/pkg/scheme"
+)
+
+// Package type metadata.
+const (
+	Group   = "palette.crossplane.io"
+	Version = "v1beta1"
+)
+
+var (
+	// SchemeGroupVersion is group version used to register these objects
+	SchemeGroupVersion = schema.GroupVersion{Group: Group, Version: Version}
+
+	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
+	SchemeBuilder = &scheme.Builder{GroupVersion: SchemeGroupVersion}
+)
+
+func init() {
+	// This is a placeholder package for cluster-scoped v1beta1 APIs
+	// The actual v1beta1 types are in the root apis/v1beta1 package
+}
---
+++ apis/namespaced/v1alpha1/register.go
@@ -0,0 +1,29 @@
+/*
+Copyright 2022 Upbound Inc.
+*/
+
+package v1alpha1
+
+import (
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"sigs.k8s.io/controller-runtime/pkg/scheme"
+)
+
+// Package type metadata.
+const (
+	Group   = "palette.m.crossplane.io"
+	Version = "v1alpha1"
+)
+
+var (
+	// SchemeGroupVersion is group version used to register these objects
+	SchemeGroupVersion = schema.GroupVersion{Group: Group, Version: Version}
+
+	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
+	SchemeBuilder = &scheme.Builder{GroupVersion: SchemeGroupVersion}
+)
+
+func init() {
+	// This is a placeholder package for namespaced-scoped v1alpha1 APIs
+	// The actual v1alpha1 types are in the root apis/v1alpha1 package
+}
