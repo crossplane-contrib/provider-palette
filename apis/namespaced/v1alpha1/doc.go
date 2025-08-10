e0b7c5c9a6d2c081ed56d4633e1499bf1c2f3945
Pavan Sokke Nagaraj <pavansn8@gmail.com>
2025-08-10 14:45:10 -0400

    Update group name for Palette provider CRDs and add new ClusterProviderConfig and ProviderConfig resources
    Signed-off-by: Pavan Sokke Nagaraj <pavansn8@gmail.com>

--- apis/namespaced/v1alpha1/doc.go
+++ apis/namespaced/v1alpha1/doc.go
@@ -4,6 +4,6 @@ Copyright 2021 Upbound Inc.
 
 // Package v1alpha1 contains the core resources of the palette jet provider.
 // +kubebuilder:object:generate=true
-// +groupName=palette.crossplane.io
+// +groupName=palette.m.crossplane.io
 // +versionName=v1alpha1
 package v1alpha1
