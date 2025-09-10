3524f49f8e2d7baee715ce685b6739e186e5e447
Pavan Sokke Nagaraj <pavansn8@gmail.com>
2025-09-09 20:32:20 -0400

    chore: make generate
    Signed-off-by: Pavan Sokke Nagaraj <pavansn8@gmail.com>

--- apis/cluster/cloudaccount/v1alpha1/zz_aws_types.go
+++ apis/cluster/cloudaccount/v1alpha1/zz_aws_types.go
@@ -21,7 +21,7 @@ type AwsInitParameters struct {
 
 	// (String) The AWS access key used to authenticate.
 	// The AWS access key used to authenticate.
-	AwsAccessKey *string `json:"awsAccessKey,omitempty" tf:"aws_access_key,omitempty"`
+	AwsAccessKeySecretRef *v1.SecretKeySelector `json:"awsAccessKeySecretRef,omitempty" tf:"-"`
 
 	// (String, Sensitive) The AWS secret key used in conjunction with the access key for authentication.
 	// The AWS secret key used in conjunction with the access key for authentication.
@@ -65,10 +65,6 @@ type AwsObservation struct {
 	// The Amazon Resource Name (ARN) associated with the AWS resource. This is used for identifying resources in AWS.
 	Arn *string `json:"arn,omitempty" tf:"arn,omitempty"`
 
-	// (String) The AWS access key used to authenticate.
-	// The AWS access key used to authenticate.
-	AwsAccessKey *string `json:"awsAccessKey,omitempty" tf:"aws_access_key,omitempty"`
-
 	// (String) The context of the AWS configuration. Allowed values are project or tenant. Default value is project. If  the project context is specified, the project name will sourced from the provider configuration parameter project_name.
 	// The context of the AWS configuration. Allowed values are `project` or `tenant`. Default value is `project`. If  the `project` context is specified, the project name will sourced from the provider configuration parameter [`project_name`](https://registry.io/providers/spectrocloud/spectrocloud/latest/docs#schema).
 	Context *string `json:"context,omitempty" tf:"context,omitempty"`
@@ -110,7 +106,7 @@ type AwsParameters struct {
 	// (String) The AWS access key used to authenticate.
 	// The AWS access key used to authenticate.
 	// +kubebuilder:validation:Optional
-	AwsAccessKey *string `json:"awsAccessKey,omitempty" tf:"aws_access_key,omitempty"`
+	AwsAccessKeySecretRef *v1.SecretKeySelector `json:"awsAccessKeySecretRef,omitempty" tf:"-"`
 
 	// (String, Sensitive) The AWS secret key used in conjunction with the access key for authentication.
 	// The AWS secret key used in conjunction with the access key for authentication.
